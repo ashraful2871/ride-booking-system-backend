19e57c486ebe7298267a0491664a141e97ac308d
ashraful2871 <ashrafulislam2758@gmail.com>
2025-08-01 17:50:35 +0600

    secure the api route for super admin

--- src/app/modules/admin/admin.route.ts
+++ src/app/modules/admin/admin.route.ts
@@ -1,12 +1,34 @@
 import { Router } from "express";
 import { adminController } from "./admin.controller";
+import { checkAuth } from "../../middlewares/checkAuth";
+import { Role } from "../user/user.interface";
 
 const router = Router();
 
-router.get("/all-users", adminController.getAllUsers);
-router.get("/all-rides", adminController.getAllRides);
-router.get("/all-drivers", adminController.getAllDriver);
-router.patch("/driver-status/:driverId", adminController.driverApprovedStatus);
-router.patch("/block-user/:userId", adminController.blockUser);
+router.get(
+  "/all-users",
+  checkAuth(Role.SUPER_ADMIN),
+  adminController.getAllUsers
+);
+router.get(
+  "/all-rides",
+  checkAuth(Role.SUPER_ADMIN),
+  adminController.getAllRides
+);
+router.get(
+  "/all-drivers",
+  checkAuth(Role.SUPER_ADMIN),
+  adminController.getAllDriver
+);
+router.patch(
+  "/driver-status/:driverId",
+  checkAuth(Role.SUPER_ADMIN),
+  adminController.driverApprovedStatus
+);
+router.patch(
+  "/block-user/:userId",
+  checkAuth(Role.SUPER_ADMIN),
+  adminController.blockUser
+);
 
 export const adminRoutes = router;
