44062bfab7547b8cabd5644f9df5e29eb6243ad2
ashraful2871 <ashrafulislam2758@gmail.com>
2025-08-02 16:03:30 +0600

    change some fixex

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,8 +2,14 @@ import { envVars } from "../../config/env";
 import { IAuthProvider, IUser } from "./user.interface";
 import bcryptjs from "bcryptjs";
 import { User } from "./user.model";
+import AppError from "../../errorHelper/appError";
+import { StatusCodes } from "http-status-codes";
 const createUser = async (payload: Partial<IUser>) => {
   const { email, password, ...rest } = payload;
+  const isUserExist = await User.findOne({ email: email });
+  if (isUserExist) {
+    throw new AppError(StatusCodes.BAD_REQUEST, "User Already Exist");
+  }
   const hasPassword = await bcryptjs.hash(
     password as string,
     Number(envVars.BCRYPT_SALT_ROUND)
